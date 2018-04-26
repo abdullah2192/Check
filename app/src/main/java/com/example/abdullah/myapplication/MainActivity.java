e97a754edfd65e0f337e6e11ad72f36b2ef6a785
Abdullah <Abdullah@aptechnn.edu>
2018-04-26 10:29:32 +0500

    Initial commit

---
+++ app/src/main/java/com/example/abdullah/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.abdullah.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class MainActivity extends AwesomeSplash {
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        configSplash.setLogoSplash(R.mipmap.ic_launcher); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce);
+
+        configSplash.setTitleSplash("My Awesome App");
+        configSplash.setTitleTextColor(R.color.colorPrimaryDark);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+
+    }
+
+    @Override
+    public void animationsFinished() {
+      //  Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show();
+    }
+}
